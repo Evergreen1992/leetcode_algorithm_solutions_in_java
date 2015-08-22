e6eb1fe99e9df3518d9e1c01cabcf69814dbba8f
Evergreen1992 <781804951@qq.com>
2015-08-22 17:09:48 +0800

    牛客网面试题算法实现--寻找最小缺失整数。

---
+++ src/niu_ke_net/MissNum.java
@@ -0,0 +1,37 @@
+package niu_ke_net;
+
+/**
+ 
+ * @author Evergreen
+ 数组中未出现的最小正整数
+【题目】
+给定一个无序整型数组arr，找到数组中未出现的最小正整数。 
+【举例】
+arr=[-1,2,3,4]。返回1。
+arr=[1,2,3,4]。返回5。
+ *
+ */
+public class MissNum {
+    
+    public static int misNum(int[] arr){
+	int l = 0 ;
+	int r = arr.length;
+	while( l < r){
+	    if( arr[l] == l + 1){
+		l ++ ;
+	    }else if( arr[l] <= l || arr[l] > r || arr[arr[l] - 1] == arr[l]){
+		arr[l] = arr[-- r];
+	    }else{
+		//swap(arr, l, arr[l] - 1);
+	    }
+	}
+	return l + 1;
+    }
+    
+
+    public static void main(String[] args) {
+	int[] arr = new int[]{1,1,3,4};
+	System.out.println(misNum(arr));
+    }
+
+}
