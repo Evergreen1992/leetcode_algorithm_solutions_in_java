d27784f344e98ab53f434d077dd048f12a95dc6b
Evergreen1992 <781804951@qq.com>
2015-09-23 23:04:33 +0800

    not passed

---
+++ src/美团2016/Solution.java
@@ -0,0 +1,20 @@
+package 美团2016;
+
+/**
+ 每组数据包涵s1（长度小于100），s2（长度小于100），len1（小于100000），len2（大于len1，小于100000）
+输出描述:
+输出答案。
+输入例子:
+ab ce 1 2
+输出例子:
+56
+ * @author Administrator
+ *
+ */
+public class Solution {
+    
+    public static void main(String a, String b) {
+	System.out.println();
+    }
+
+}
