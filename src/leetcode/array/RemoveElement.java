896336838d865eb31e27cf1de601a277a42cab5a
xiongxiao <xiongxiao@xiongxiao-ThinkPad-E420>
2015-09-27 18:40:01 +0800

    remove elements

---
+++ src/leetcode/array/RemoveElement.java
@@ -0,0 +1,18 @@
+package leetcode.array;
+
+public class RemoveElement {
+	
+	public int removeElement(int[] A, int elem) {
+        int i = 0 , j = 0 ;
+        for( i = 0 ; i < A.length; i ++){
+        	if( A[i] != elem)
+        		A[j ++ ] = A[i];
+        }
+        return j ;
+    }
+
+	public static void main(String[] args) {
+
+	}
+
+}
