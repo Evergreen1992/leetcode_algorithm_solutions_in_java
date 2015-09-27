49e55da49d763f0bb81787dc256d58e50e222bda
xiongxiao <xiongxiao@xiongxiao-ThinkPad-E420>
2015-09-27 18:16:10 +0800

    rotate image

---
+++ src/leetcode/array/RotateImage.java
@@ -0,0 +1,41 @@
+package leetcode.array;
+
+public class RotateImage {
+	
+	public static void rotate(int[][] matrix){
+		
+		int  size = matrix.length;
+		int[][] newA = new int[size][size];
+		
+		for( int i = 0 ; i < size ; i ++){
+			for( int j = 0 ; j < size ; j ++){
+				newA[j][size - 1 - i] = matrix[i][j];
+			}
+		}
+		
+		for( int i = 0 ; i < newA.length; i ++){
+			for( int j = 0 ; j < newA[i].length; j ++){
+				matrix[i][j] = newA[i][j];
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[][] matrix = {
+				{1,2},
+				{3,4}
+				//31
+				//42
+		};
+		
+		rotate(matrix);
+		
+		for( int i = 0 ; i < matrix.length; i ++){
+			for( int j = 0 ; j < matrix[i].length; j ++){
+				System.out.print(matrix[i][j] + " , ");
+			}
+			System.out.println();
+		}
+	}
+
+}
