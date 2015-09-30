3523e13b67dcebfede6c627f859c5b90a6d56d9d
Evergreen1992 <781804951@qq.com>
2015-09-30 22:10:03 +0800

    set matrix zeroes

---
+++ src/leetcode/array/SetMatrixZeroes.java
@@ -0,0 +1,15 @@
+package leetcode.array;
+
+public class SetMatrixZeroes {
+    public static void setZeroes(int[][] matrix) {
+    }
+
+	public static void main(String[] args) {
+		int[][] array = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		};
+	}
+
+}
