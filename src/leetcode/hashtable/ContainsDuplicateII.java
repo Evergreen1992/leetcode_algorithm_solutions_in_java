4fc0823664e70afd2a41daf7d272ebc129df2947
Evergreen1992 <781804951@qq.com>
2015-12-11 13:39:04 +0800

    contains duplicate II

---
+++ src/leetcode/hashtable/ContainsDuplicateII.java
@@ -0,0 +1,22 @@
+package leetcode.hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+	public static boolean containsNearbyDuplicate(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for( int i = 0 ; i < nums.length; i ++){
+			if( map.containsKey(nums[i])){
+				if( i - map.get(nums[i]) <= k)
+					return true ;
+			}
+			map.put(nums[i], i);
+		}
+		return false ;
+    }
+
+	public static void main(String[] args) {
+		System.out.println(containsNearbyDuplicate(new int[]{0,11,2,11,1,5,6,7,8,9}, 5));
+	}
+}
