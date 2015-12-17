92373e641d20dea13e60fee2ad85bd758a9da3d8
Evergreen1992 <781804951@qq.com>
2015-12-17 20:43:15 +0800

    use o(m + n) space

--- src/leetcode/array/SetMatrixZeroes.java
+++ src/leetcode/array/SetMatrixZeroes.java
@@ -2,14 +2,39 @@ package leetcode.array;
 
 public class SetMatrixZeroes {
     public static void setZeroes(int[][] matrix) {
+    	if( matrix.length == 0)
+    		return ;
+    	int rows[] = new int[matrix.length];
+    	int columns[] = new int[matrix[0].length];
+    	
+    	for( int i = 0 ; i < matrix.length; i ++){
+    		for( int j = 0 ; j <matrix[i].length; j ++){
+    			if( matrix[i][j] == 0 ){
+    				rows[i] = 1 ;
+    				columns[j] = 1 ;
+    			}
+    		}
+    	}
+    	for( int i = 0 ; i < matrix.length; i ++){
+    		for( int j = 0 ; j < matrix[i].length; j ++){
+    			if( columns[j] == 1 || rows[i] == 1)
+    				matrix[i][j] = 0 ;
+    		}
+    	}
     }
 
 	public static void main(String[] args) {
 		int[][] array = {
-				{1, 2, 3},
-				{4, 5, 6},
+				{0, 2, 3},
+				{4, 0, 6},
 				{7, 8, 9}
 		};
+		setZeroes(array);
+		for( int[] outer : array){
+			for( int item : outer){
+				System.out.print(item + " , ");
+			}
+			System.out.println();
+		}
 	}
-
 }
