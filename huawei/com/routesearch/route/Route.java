7db7c9d7fb5d31d41c3aeb9256e1f36c10e4a5a3
Evergreen1992 <781804951@qq.com>
2016-03-15 00:24:46 +0800

    初步结果

--- huawei/com/routesearch/route/Route.java
+++ huawei/com/routesearch/route/Route.java
@@ -7,7 +7,9 @@
  */
 package com.routesearch.route;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -37,6 +39,8 @@ public final class Route
 	static int start , end ;
 	//要经过的节点
 	static String[] conditionNodes = null ;
+	
+	static List<Integer> visitedNodes = new ArrayList<Integer>();//访问过的节点
     /**
      * 你需要完成功能的入口
      * 
@@ -48,11 +52,11 @@ public final class Route
     {
     	String[] items ;
     	Node node = null ;
-    	matrix = new int[graphContent.split("\n").length][4];
+    	matrix = new int[graphContent.split("\n").length][graphContent.split("\n").length];
     	start = Integer.parseInt(condition.split(",")[0]);
     	end = Integer.parseInt(condition.split(",")[1]);
     	conditionNodes = (condition.split(",")[2]).split("\\|");
-    	
+    	System.out.println(matrix.length + "," + matrix[0].length);
     	for(String item : graphContent.split("\n")){
     		items = item.split(",");
     		matrix[Integer.parseInt(items[1])][Integer.parseInt(items[2])] = Integer.parseInt(items[3]);
@@ -69,15 +73,84 @@ public final class Route
     		}
     	}
     	
-    	runAlgorithm();
     	
+    	
+    	
+    	System.out.println("访问节点:" + start);
+    	
+    	visitedNodes.add(start);
+    	findNodes(start);
         return "hello world!";
     }
 
     /**
      * 开始执行算法
      */
-    public static void runAlgorithm(){
-    	String path ;//节点路径
+    public static void findNodes(int nodeIndex){
+    	//遍历访问当前行。
+    	int emptySize = 0 ;
+    	for( int index = 0 ; index < matrix[nodeIndex].length; index ++){
+    		if( matrix[nodeIndex][index] >= 1){//存在邻接节点
+    			System.out.println("访问节点:" + nodeIndex + ":" + index);
+    			
+    			//检测节点是否已经访问过
+    			if(visitedNodes.contains(index) == true){
+    				continue;
+    			}
+    			
+    			//检测是否到达终点
+    			if( index == end ){
+    				if( throughNodes() == true){
+    					visitedNodes.add(end);
+	    				System.out.println("访问结束~~~~~~~~~~~到达终点");
+	    				handleResult();
+	    				return ;
+    				}else{
+    					//无效的节点，移除
+    					visitedNodes.remove(visitedNodes.size() - 1);
+    					System.out.println("~~~~~~~~~~~到达终点，但不满足经过节点条件" + index);
+    					continue;
+    				}
+    			}else{
+    				visitedNodes.add(index);
+    				findNodes(index);
+    			}
+    		}else{
+    			emptySize ++ ;
+    		}
+    	}
+    	
+    	if( emptySize == 4){
+    		System.out.println("节点->" + nodeIndex + " 无效");
+    		//不可达路径
+    		visitedNodes.remove(visitedNodes.size() - 1);
+    	}
+    }
+    /**
+     * 判断是否经过所有要求的节点
+     * @return
+     */
+    public static boolean throughNodes(){
+    	//System.out.println("-----------------------------------------");
+    	for( int ii : visitedNodes){
+    		System.out.print(ii + "->");
+    	}
+    	//System.out.println("------------------------------------------");
+    	for(String nodeIndex : conditionNodes){
+    		if( visitedNodes.contains(Integer.parseInt(nodeIndex.trim())) == false)
+    			return false;
+    	}
+    	return true;
+    }
+    
+    /**
+     * 处理最终结果
+     */
+    public static void handleResult(){
+    	System.out.println("*****************-********************");
+    	for(int item : visitedNodes){
+    		System.out.print(item + "->");
+    	}
+    	System.out.println();
     }
 }
